1f6d2727d69c9a9509c0f84dd89aac0e78e0da4a
eworman42 <eworman@ualberta.ca>
2017-08-29 10:36:11 -0600

    Typo
    "Hillary Clinton" was an entry but "Crooked Hillary" was not. Fixed this glaring mistake.

--- trumplibs2.R
+++ trumplibs2.R
@@ -8,16 +8,17 @@ trump_libs <- function(...){
                  'reckless', 'sad', 'sick', 'slanderous', 'slanted', 'sleazy', 'sleepy', 'small', 'stinking', 'stupid', 'terrible', 'violent', 'tired', 'unpopular',
                  'unqualified', 'untalented', 'violent', 'wacko', 'weak', 'worst', 'worthless') 
     enemies = c('Adam Schiff', 'Antifa', 'Al Franken', 'Alicia Machado', 'Anderson Cooper', 'Anthony Weiner', 'Arnold Schwartzenegger', 'Barack Obama',
-                'Ben Carson', 'Bernie Sanders', 'Bill Clinton', 'Bill Kristol', 'Bobby Jindal', 'Carly Fiorina', 'Chris Christie', 'Chuck Jones', 
-                'Chuck Schumer', 'Chuck Todd', 'CNN', 'Cokie Roberts', 'Colin Powell', 'David Brooks', 'Debbie Wasserman Schultz', 'Democrats', 'DNC', 
+                'Ben Carson', 'Bernie Sanders', 'Bill Clinton', 'Bill Kristol', 'Black Lives Matter', 'Bobby Jindal', 'Carly Fiorina', 'Chris Christie', 'Chuck Jones', 
+                'Chuck Schumer', 'Chuck Todd', 'CNN', 'Cokie Roberts', 'Colin Powell', 'Crooked Hillary', 'David Brooks', 'Debbie Wasserman Schultz', 'Democrats', 'DNC', 
                 'Donna Brazile', 'Elizabeth Warren', 'EPA', 'Feminazis', 'George Pataki', 'George W. Bush', 'Glenn Beck', 'Hezbollah', 'Hillary Clinton', 'Huma Abedin', 'Iran', 'James Clapper', 
-                'James Comey', 'Jeb Bush', 'Jeff Flake', 'Joe Biden', 'Joe Scarborough', 'John Kasich', 'John McCain', 'John Oliver', 'John Podesta', 
-                'Jon Ossoff', 'Juan Williams', 'Kathy Griffin', 'Katie Couric', 'Lawrence O\'Donnell', 'Lebanon', 'Loretta Lynch', 'Marco Rubio', 'Mark Cuban', 
+                'James Comey', 'Jeb Bush', 'Jeff Flake', 'Jews', 'Joe Biden', 'Joe Scarborough', 'John Kasich', 'John McCain', 'John Oliver', 'John Podesta', 
+                'Jon Ossoff', 'Juan Williams', 'Kathy Griffin', 'Katie Couric', 'Lawrence O\'Donnell', 'Lebanon', 'Loretta Lynch', 'Marco Rubio', 'Mainstream Media', 'Mark Cuban', 
                 'Megyn Kelly', 'Mexico', 'Michael Bloomberg', 'Mika Brzezinski', 'Mitt Romney', 'MSNBC', 'Nancy Pelosi', 'New York Times', 'Penn Jillette', 
                 'Rand Paul', 'Rick Perry', 'Robert Gates', 'Robert Mueller', 'Ronda Rousey', 'Ruth Bader Ginsburg', 'Sadiq Khan', 'Sally Yates', 
                 'Samuel L. Jackson', 'Scott Walker', 'Sid Blumenthal', 'Ted Cruz', 'Boyscouts of America', 'Tim Kaine', 'Vincente Fox', 'Washington Post', 'Whoopi Goldberg') 
     adjective = c('SAD!', 'BAD!', 'TERRIBLE!', 'APOLOGIZE!', 
-                  'COVFEFE!', 'HEEL!', 'MANY SIDES!', 'Our poor country!')
+                  'COVFEFE!', 'HEEL!', 'MANY SIDES!', 'FAKE NEWS!', 'Our poor country!',
+                  'SHAME!')
     reason = c('was complaining about my wall', 'is using me for their own publicity, they are third rate!', 
                'is constantly seeking out trivial nonsense and wasting taxpayer dollars.', 'has got a LOT of problems!', 'is failing!', 
                'graduated last in their class, a total dummy!', 'is desperate and has no imagination!', 'wants to take Jesus out of Christmas!', 
